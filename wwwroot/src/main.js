bdc7b727b142a7e79f6189217b079ce036c56f2c
Johann Bączkiewicz <johann.baczkiewicz@gmail.com>
2018-03-18 01:05:38 +0100

    rename test2.vue -> App.vue

--- wwwroot/src/main.js
+++ wwwroot/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from './test2.vue'
+import App from './App.vue'
 import Users from './pages/Users.vue'
 import Home from './pages/Home.vue'
 import About from './pages/About.vue'
